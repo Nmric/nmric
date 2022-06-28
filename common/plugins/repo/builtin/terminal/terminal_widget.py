0c47bd2ebd7be4cd77c3c6fae9f207ba828f19f5
Menno Nijboer <menno.nijboer@slimmer.ai>
2022-06-28 10:43:42 +0200

    chore: working out websockets

--- common/plugins/repo/builtin/terminal/terminal_widget.py
+++ common/plugins/repo/builtin/terminal/terminal_widget.py
@@ -1,5 +1,8 @@
 import time
 
+import zmq
+from flask import make_response, Response
+
 from common.plugins.core import DisplayPlugin
 
 class TerminalWidget(DisplayPlugin):
@@ -7,8 +10,8 @@ class TerminalWidget(DisplayPlugin):
         description = "The standard terminal widget to send g-code to the connected machine"
         super().__init__(description=description)
 
-    def render(self) -> str:
-        return """
+    def render(self) -> Response:
+        template = """
         <div hx-ws="connect:/plugins/stream/builtin.terminal">
             <div id="terminal_output">
                 ...
@@ -19,10 +22,16 @@ class TerminalWidget(DisplayPlugin):
         </div>
         """
 
-    def stream(self, ws) -> str:
-        result = ""
-        i = 0
-        n = 0
+        return make_response(template, 200)
+
+    def stream(self, websocket) -> str:
+        machine_host = "127.0.0.1"
+        machine_port = 6668
+
+        socket = zmq.Context().socket(zmq.REQ)
+        socket.connect('tcp://{}:{}'.format(machine_host, machine_port))
+        socket.setsockopt(zmq.RCVTIMEO, 2000)
+
         while True:
             i += 1
             if i > 0:
@@ -31,5 +40,20 @@ class TerminalWidget(DisplayPlugin):
                 result += f"<b>{n}</b><br>" 
                 # data = ws.receive()
                 print("ECHO ", n)
-                ws.send(f"<div id='terminal_output' hx-swap-oob='true'>{result}</div>")
+                websocket.send(f"<div id='terminal_output' hx-swap-oob='true'>{result}</div>")
             time.sleep(2)
+
+    # def stream(self, ws) -> str:
+    #     result = ""
+    #     i = 0
+    #     n = 0
+    #     while True:
+    #         i += 1
+    #         if i > 0:
+    #             i = 0
+    #             n += 1
+    #             result += f"<b>{n}</b><br>" 
+    #             # data = ws.receive()
+    #             print("ECHO ", n)
+    #             ws.send(f"<div id='terminal_output' hx-swap-oob='true'>{result}</div>")
+    #         time.sleep(2)
